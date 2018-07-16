196c927c3a57ff0e198235e02eff98c1170128a2
Tomáš Celizna <tomas.celizna@gmail.com>
2018-07-17 00:03:03 +0200

    cleanup

--- lib/simple_form_recurring_select.rb
+++ lib/simple_form_recurring_select.rb
@@ -2,8 +2,6 @@ require 'simple_form_recurring_select/has_recurrence_rule'
 require 'simple_form_recurring_select/recurring_select_input'
 require 'simple_form_recurring_select/version'
 
-# ---------------------------------------------------------------------
-
 module SimpleForm
   class FormBuilder
     map_type :recurring_select, to: SimpleFormRecurringSelect::RecurringSelectInput
